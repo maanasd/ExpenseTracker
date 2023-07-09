fae6a2178262741749743d84ac1ceba6610399c3
maanasd <maanasdatta@gmail.com>
2023-07-09 15:59:06 -0600

    added styling and radio buttons for create page

--- Expense Tracker/Program.cs
+++ Expense Tracker/Program.cs
@@ -10,6 +10,9 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
+//Register Syncfusion license
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBMAY9C3t2V1hhQlJAfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn5UdkxiX35dcnNVT2Fa");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
